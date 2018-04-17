2bee7d2a0fac4417d3eb81e4eb802f7449232d1d
niceboy76 <7282830+niceboy76@users.noreply.github.com>
2018-04-17 16:16:33 +0200

    Update and rename Password.uc.js to DesktopFiles.uc.js

---
+++ firefox_toolbarbuttons/firefox_toolbarbuttons/DesktopFiles.uc.js
@@ -0,0 +1,32 @@
+
+  //  DesktopFiles.uc.js
+
+  (function() {
+
+   if (location != 'chrome://browser/content/browser.xul') return;
+
+	try {
+                CustomizableUI.createWidget({
+                        id: 'Desktop Files',
+                        type: 'custom',
+                        defaultArea: CustomizableUI.AREA_NAVBAR,
+                        onBuild: function(aDocument) {         
+                                var toolbaritem = aDocument.createElementNS('http://www.mozilla.org/keymaster/gatekeeper/there.is.only.xul', 'toolbarbutton');
+                                var props = {
+                                        id: 'Desktop Files',
+                                        class: 'toolbarbutton-1 chromeclass-toolbar-additional',
+                                        label: 'Desktop Files',
+                                        tooltiptext: 'Desktop Files',
+                                        style: 'list-style-image: url(file:///C:/Users/Fabrice%20Copparoni/AppData/Roaming/Mozilla/Firefox/Profiles/drhot95b.FX57+/chrome/image/Ic%C3%B4nes/CB/128.png)',
+				        onclick: 'if (event.button == 0) { \
+							           		openUILinkIn("file:///c:/Users/Fabrice%20Copparoni/Desktop/", "tab");\
+								 }; '
+                                };            
+                                for (var p in props)
+                                        toolbaritem.setAttribute(p, props[p]);            
+                                return toolbaritem;
+			}
+		});
+	} catch(e) { };
+
+})();
